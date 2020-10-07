3fb818fad302b9a22f395464fdf7ff8c7e1bd40c
Leroy Clarke Jr <lclarkejr94@gmail.com>
2020-10-07 09:32:37 -0400

    progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/styles.css";
+
+//COMPONENTS
+// import Button from "./components/button.js";
+import Header from "./components/header.js";
+import About from "./components/about";
+import BirthdayFacts from "./components/birthdayfacts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <Header />
+      <section id="hero">
+        <div>
+          <h2>
+            There are so many things to be grateful for today... I couldntbe
+            anymore excited about the fact that we havent gotten
+          </h2>
+
+          <h2>But really, it's amazing what.</h2>
+          {/* <Button label="Let us show you the facts!" /> */}
+        </div>
+      </section>
+      <About />
+      <BirthdayFacts />
     </div>
   );
 }
